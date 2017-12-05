6c3315b3893da02ca19aff10a3c78ef119900ea6
Shahzaib Shahid <shahzaib.shahid414@gmail.com>
2017-12-05 14:14:42 +0800

    first Commit

---
+++ app/src/main/java/org/cfp/citizenconnect/Notification/FCMService.java
@@ -0,0 +1,11 @@
+package org.cfp.citizenconnect.Notification;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by root on 05/12/2017.
+ */
+
+public class FCMService extends FirebaseMessagingService {
+
+}
